8238f59f4dfd22a30e5133f891e2a59d04dbdcdf
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-01-05 14:48:16 +0530

    Updated AboutMe Section

--- src/screens/AboutMe.tsx
+++ src/screens/AboutMe.tsx
@@ -17,11 +17,16 @@ const AboutMe = () => {
                     <Typography component="h2" variant="h4" fontWeight={500}>About Me</Typography>
                 </Grid>
                 <Grid item xs={12}>
-                    <Avatar src='https://avatars.githubusercontent.com/u/117020521?v=4' sx={{ width: '15vw', height: '15vw' }} />
+                    <Avatar src='https://avatars.githubusercontent.com/u/117020521?v=4' sx={{ width: '12vw', height: '12vw' }} />
                 </Grid>
                 <Grid item xs={12}>
                     <Typography component="h3" variant={isXs ? "body1" : "h6"} fontWeight={400}>
-                        "As a mechanical engineer, my education initially centered around the physical world. However, the curiosity to explore the virtual realm and understand how things work in the digital space led me to the exciting world of coding. Since that pivotal moment, I have embraced coding wholeheartedly, embarking on a transformative journey as a developer. Today, I specialize in turning concepts into reality as a full-stack web and mobile app developer."
+                        "As a passionate Web & App Developer with one year of hands-on experience in front-end development, I specialize in React, React Native, JavaScript/TypeScript, Node.js, and Express. My creative and adaptable approach focuses on building engaging user interfaces and scalable backends, leveraging technologies like Redux, Material UI, React Native Paper, and databases such as MongoDB and MySQL."
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography component="h3" variant={isXs ? "body1" : "h6"} fontWeight={400}>
+                        "My journey began as a mechanical engineer, and my curiosity to explore the virtual realm led me to coding. Embracing it wholeheartedly, I've transformed into a full-stack developer, dedicated to turning concepts into reality. Excited about the journey ahead, I look forward to crafting meaningful digital experiences."
                     </Typography>
                 </Grid>
             </Grid>
