24b9fd537b557b3c80496d371d2402457154ac29
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-01-10 21:00:43 +0530

    Projects Data Added

--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Typography } from '@mui/material';
 import { useMediaQuery } from 'react-responsive';
 import ProjectCard from '../components/ProjectCard';
+import FuelCalculatorImg from '../assets/images/Fuel_Calculator.png';
 
 const Projects = () => {
 
@@ -11,6 +12,35 @@ const Projects = () => {
     const isSm = useMediaQuery({ query: '(min-width: 600px) and (max-width: 959px)' });
     const isXs = useMediaQuery({ query: '(min-width: 320px) and (max-width: 599px)' });
 
+    const projects = [
+        {
+            projectImgURL: FuelCalculatorImg,
+            projectName: 'Fuel Calculator',
+            projectDescription: 'Fuel Calculator is a web app that efficiently estimates fuel consumption and costs before your trip, helping you plan with confidence based on current fuel prices.',
+            projectTechStack: [
+                'React',
+                'TypeScript',
+                'Material-UI',
+                'CSS'
+            ],
+            projectLink: 'https://nadeem-abdun.github.io/fuel-calculator-app/',
+            githubRepoLink: 'https://github.com/Nadeem-Abdun/fuel-calculator-app'
+        },
+        {
+            projectImgURL: 'https://nadeem-abdun.github.io/find-my-chef/static/media/HeaderNavLogo.f796b4f86a0b2cb06f7e.png',
+            projectName: 'Find My Chef!',
+            projectDescription: 'Find My Chef! - Your ultimate classified site connecting restaurant owners with top-tier chefs. Empowering chefs to discover dream jobs and assisting restaurant owners in finding the perfect culinary talent for their establishments.',
+            projectTechStack: [
+                'React',
+                'TypeScript',
+                'Material-UI',
+                'CSS'
+            ],
+            projectLink: 'https://nadeem-abdun.github.io/find-my-chef/',
+            githubRepoLink: 'https://github.com/Nadeem-Abdun/find-my-chef'
+        }
+    ]
+
     return (
         <div className={`flex flex-col justify-center items-center ${(isXs || isSm || isMd) ? 'my-3' : 'my-10'}`}>
             <Grid container xs={12} justifyContent='flex-start' alignItems='center' rowSpacing={2}>
@@ -18,7 +48,20 @@ const Projects = () => {
                     <Typography component="h2" variant="h4" fontWeight={500} fontFamily='inter'>Projects</Typography>
                 </Grid>
                 <Grid item xs={12}>
-                    <ProjectCard />
+                    {projects && projects.map((project, index) => {
+                        const { projectImgURL, projectName, projectDescription, projectTechStack, projectLink, githubRepoLink } = project;
+                        return (
+                            <ProjectCard
+                                key={index}
+                                projectImgURL={projectImgURL}
+                                projectName={projectName}
+                                projectDescription={projectDescription}
+                                projectTechStack={projectTechStack}
+                                projectLink={projectLink}
+                                githubRepoLink={githubRepoLink}
+                            />
+                        )
+                    })}
                 </Grid>
             </Grid>
         </div>
