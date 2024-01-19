0f609eb59067999a7e4be0a5799a6341ae20802d
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-01-19 13:22:21 +0530

    Contact Page Grid System And Loader Rendering Pattern Changed

--- src/screens/ContactMe.tsx
+++ src/screens/ContactMe.tsx
@@ -18,16 +18,16 @@ const ContactMe = () => {
         message: ''
     });
 
-    const [formLoading, setformLoading] = useState({
+    const [formStatus, setFormStatus] = useState({
         success: false,
         loading: false,
     });
 
-    const formSubmitLoading = async () => {
-        await setformLoading({ ...formLoading, loading: true, success: false, });
+    const handleFormStatus = async () => {
+        await setFormStatus({ ...formStatus, loading: true, success: false, });
         setTimeout(() => {
-            setformLoading({ ...formLoading, loading: false, success: true, });
-        }, 2000);
+            setFormStatus({ ...formStatus, loading: false, success: true, });
+        }, 2500);
     }
 
     const handleTextFeildsChange = (name: string, value: string) => {
@@ -43,84 +43,97 @@ const ContactMe = () => {
         await formData.append('email', contactDetails.email);
         await formData.append('contactNumber', contactDetails.contactNumber);
         await formData.append('message', contactDetails.message);
-        await formSubmitLoading();
+        await handleFormStatus();
     };
 
     return (
         <div id='section-contact-me' className={`flex flex-col justify-center items-center ${(isXs || isSm || isMd) ? 'my-3' : 'my-10'}`}>
             <Grid container xs={12} justifyContent='flex-start' alignItems='center' rowSpacing={2}>
-                <Grid item xs={12}>
-                    <Typography component="h2" variant="h4" fontWeight={500} fontFamily='inter'>Contact Me</Typography>
-                </Grid>
-                {!formLoading.success ?
-                    <React.Fragment>
-                        <Grid item xs={12}>
-                            <Typography component="h2" variant="body1" fontWeight={400} fontFamily='inter'>"How about having a quick conversation? Please fill out the form below, and I'll reach out to you shortly."</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                id="name-field"
-                                label="Name"
-                                variant="filled"
-                                fullWidth
-                                sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
-                                onChange={(event) => handleTextFeildsChange('name', event.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                id="email-field"
-                                label="Email"
-                                variant="filled"
-                                fullWidth
-                                sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
-                                onChange={(event) => handleTextFeildsChange('email', event.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                id="contactNumber-field"
-                                label="Contact Number(Optional)"
-                                variant="filled"
-                                fullWidth
-                                sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
-                                onChange={(event) => handleTextFeildsChange('contactNumber', event.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                id="message-field"
-                                label="Message"
-                                variant="filled"
-                                fullWidth
-                                multiline
-                                rows={4}
-                                sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
-                                onChange={(event) => handleTextFeildsChange('message', event.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button variant="contained" color="primary" size="large" onClick={() => handleContactFormSubmit()} fullWidth disabled={formLoading.loading}>
-                                {formLoading.loading ?
-                                    <CircularProgress size={24} color='info' />
-                                    :
-                                    'Submit'
-                                }
-                            </Button>
-                        </Grid>
-                    </React.Fragment>
-                    :
-                    <Grid container item xs={12} justifyContent='center' alignItems='center' rowSpacing={isXs ? 1 : 2}>
-                        <Grid item xs={12}>
-                            <img src={SendSuccessfullyLogo} className='h-80 mx-auto' alt='Form_Submitted' />
-                        </Grid>
-                        <Grid item xs={12} className="text-center">
-                            <Typography component='h2' variant={isXs ? 'body1' : 'h6'} fontWeight={400} fontFamily='inter'>
-                                Form submitted successfully and I'll reach out to you shortly.
-                            </Typography>
-                        </Grid>
+                <Grid container item xs={12} justifyContent='flex-start' alignItems='center'>
+                    <Grid item xs={12}>
+                        <Typography component="h2" variant="h4" fontWeight={500} fontFamily='inter'>Contact Me</Typography>
                     </Grid>
-                }
+                </Grid>
+                <Grid container item xs={12} justifyContent='flex-start' alignItems='center'>
+                    {!formStatus.success && !formStatus.loading &&
+                        <Grid container item xs={12} rowSpacing={2}>
+                            <Grid item xs={12}>
+                                <Typography component="h2" variant="body1" fontWeight={400} fontFamily='inter'>How about having a quick conversation? Please fill out the form below, and I'll reach out to you shortly.</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="name-field"
+                                    label="Name"
+                                    variant="filled"
+                                    fullWidth
+                                    sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
+                                    onChange={(event) => handleTextFeildsChange('name', event.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="email-field"
+                                    label="Email"
+                                    variant="filled"
+                                    fullWidth
+                                    sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
+                                    onChange={(event) => handleTextFeildsChange('email', event.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="contactNumber-field"
+                                    label="Contact Number(Optional)"
+                                    variant="filled"
+                                    fullWidth
+                                    sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
+                                    onChange={(event) => handleTextFeildsChange('contactNumber', event.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="message-field"
+                                    label="Message"
+                                    variant="filled"
+                                    fullWidth
+                                    multiline
+                                    rows={4}
+                                    sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
+                                    onChange={(event) => handleTextFeildsChange('message', event.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button variant="contained" color="primary" size="large" onClick={() => handleContactFormSubmit()} fullWidth>Submit</Button>
+                            </Grid>
+                        </Grid>
+                    }
+                    {formStatus.loading &&
+                        <Grid container item xs={12} rowSpacing={isXs ? 1 : 2}>
+                            <Grid item xs={12}>
+                                <div className={`${(isXl) ? 'mx-80' : (isLg || isMd) ? 'mx-40' : 'mx-20'} text-center mt-10`}>
+                                    <CircularProgress size={120} color='info' />
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} className="text-center">
+                                <Typography component='h2' variant={isXs ? 'body1' : 'h6'} fontWeight={400} fontFamily='inter'>
+                                    Please wait...
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    }
+                    {formStatus.success && !formStatus.loading &&
+                        <Grid container item xs={12} rowSpacing={isXs ? 0 : 2}>
+                            <Grid item xs={12}>
+                                <img src={SendSuccessfullyLogo} className='h-80 mx-auto' alt='Form_Submitted' />
+                            </Grid>
+                            <Grid item xs={12} className="text-center">
+                                <Typography component='h2' variant={isXs ? 'body1' : 'h6'} fontWeight={400} fontFamily='inter'>
+                                    Thank you for reaching out! Your details have been received, and I'll get back to you soon to continue our conversation.
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    }
+                </Grid>
             </Grid>
         </div >
     )
