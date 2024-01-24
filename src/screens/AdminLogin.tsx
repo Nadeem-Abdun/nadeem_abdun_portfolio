2bba825d3fa57ae0a402258bd8edc5db9d612143
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-01-24 18:53:12 +0530

    Admin Login Grid Pattern And Loading Functionality Updated

--- src/screens/AdminLogin.tsx
+++ src/screens/AdminLogin.tsx
@@ -30,10 +30,12 @@ const AdminLogin = () => {
 
     const handleFormStatus = async () => {
         await setFormLoading(true);
-        await setTimeout(() => {
+        setTimeout(() => {
             setFormLoading(false);
         }, 2500);
-        // await history('/admin/home');
+        setTimeout(() => {
+            history('/admin/home');
+        }, 2500);
     }
 
     const handleLoginFormSubmit = async () => {
@@ -44,56 +46,62 @@ const AdminLogin = () => {
     }
 
     return (
-        <div className='flex flex-col justify-center items-center'>
-            <div className={`${(isXs || isSm || isMd) ? 'mx-5 my-3' : 'mx-10 my-10'}`}>
-                <Grid container xs={12} justifyContent='center' alignItems='center' rowSpacing={(isXl || isLg || isMd) ? 5 : 3}>
-                    <Grid item xs={12} className='text-center'>
-                        <Typography component="h2" variant="h4" fontWeight={500} fontFamily='inter'>Admin Login</Typography>
+        <Grid container justifyContent='center' alignItems='center' flexDirection='column' rowSpacing={4} wrap='nowrap'>
+            <Grid item xs={12} className='text-center'>
+                <Typography component="h2" variant="h4" fontWeight={500} fontFamily='inter'>Admin Login</Typography>
+            </Grid>
+            <Grid container item xl={6} lg={6} md={8} sm={10} xs={10} justifyContent='center' alignItems='center' rowSpacing={2}>
+                {!formLoading &&
+                    <>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="username-field"
+                                label="Email or Username"
+                                variant="filled"
+                                fullWidth
+                                sx={{ backgroundColor: '#D1D5DB', borderRadius: 2, }}
+                                onChange={(event) => handleTextFeildsChange('username', event.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="password-field"
+                                label="Password"
+                                variant="filled"
+                                fullWidth
+                                sx={{ backgroundColor: '#D1D5DB', borderRadius: 2, }}
+                                onChange={(event) => handleTextFeildsChange('password', event.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                size="large"
+                                onClick={() => handleLoginFormSubmit()}
+                                fullWidth
+                            >
+                                Login
+                            </Button>
+                        </Grid>
+                    </>
+                }
+                {formLoading &&
+                    <Grid container xs={12} justifyContent='center' alignItems='center' flexDirection='column' rowSpacing={2} wrap='nowrap'>
+                        <Grid item xs={12}>
+                            <div className='mt-10 text-center'>
+                                <CircularProgress size={120} color='info' />
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} className="text-center">
+                            <Typography component='h2' variant={isXs ? 'body1' : 'h6'} fontWeight={400} fontFamily='inter'>
+                                Please wait...
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    {!formLoading &&
-                        <Grid container item xs={12} justifyContent='center' alignItems='center' flexDirection='column' rowSpacing={2}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    id="username-field"
-                                    label="Email or Username"
-                                    variant="filled"
-                                    fullWidth
-                                    sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
-                                    onChange={(event) => handleTextFeildsChange('username', event.target.value)}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    id="password-field"
-                                    label="Password"
-                                    variant="filled"
-                                    fullWidth
-                                    sx={{ backgroundColor: '#D1D5DB', borderRadius: 3, }}
-                                    onChange={(event) => handleTextFeildsChange('password', event.target.value)}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Button variant="contained" color="primary" size="large" onClick={() => handleLoginFormSubmit()} fullWidth>Submit</Button>
-                            </Grid>
-                        </Grid>
-                    }
-                    {formLoading &&
-                        <Grid container item xs={12} rowSpacing={isXs ? 1 : 2}>
-                            <Grid item xs={12}>
-                                <div className={`${(isXl) ? 'mx-80' : (isLg || isMd) ? 'mx-40' : 'mx-20'} text-center mt-10`}>
-                                    <CircularProgress size={120} color='info' />
-                                </div>
-                            </Grid>
-                            <Grid item xs={12} className="text-center">
-                                <Typography component='h2' variant={isXs ? 'body1' : 'h6'} fontWeight={400} fontFamily='inter'>
-                                    Please wait...
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    }
-                </Grid>
-            </div>
-        </div>
+                }
+            </Grid>
+        </Grid>
     )
 }
 
