af8d53a6a4be1bcf48c2b5d49735a0478638adb2
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-01-25 21:25:25 +0530

    Page Title updating functionality added - AdminHome

--- src/screens/AdminHome.tsx
+++ src/screens/AdminHome.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Typography } from '@mui/material';
 import './ScreenStyles.css';
 import { useMediaQuery } from 'react-responsive';
@@ -13,6 +13,10 @@ const AdminHome = () => {
     const isMd = useMediaQuery({ query: '(min-width: 960px) and (max-width: 1279px)' });
     const isSm = useMediaQuery({ query: '(min-width: 600px) and (max-width: 959px)' });
     const isXs = useMediaQuery({ query: '(min-width: 320px) and (max-width: 599px)' });
+
+    useEffect(() => {
+        document.title = "Admin - Home"
+    }, []);
 
     return (
         <Grid container justifyContent='center' alignItems='center' flexDirection='column' rowSpacing={4} wrap='nowrap'>
