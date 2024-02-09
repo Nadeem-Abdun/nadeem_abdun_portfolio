c194e4ef321efdee3d2fdcff89e3205182893b0c
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-09 10:44:35 +0530

    SkillCard Component Added

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Grid, Typography } from '@mui/material';
+
+interface Props {
+    icon: JSX.Element,
+    name: string
+}
+
+const SkillCard: React.FC<Props> = (props) => {
+    const { icon, name } = props;
+    return (
+        <div className="skill-card px-1 py-1">
+            <Grid container xs={12} justifyContent='center' alignItems='center'>
+                <Grid item>
+                    {icon}
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography className="text-center" variant="body1" fontWeight={400} fontFamily='inter'>{name}</Typography>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default SkillCard
