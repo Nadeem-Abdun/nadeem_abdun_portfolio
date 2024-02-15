b546bec4d1b7eb207ce92b730c3cc6ba10b68c23
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-15 20:41:11 +0530

    Responsiveness For Web Navigation Panel Improved

--- src/screens/WebNavigation.tsx
+++ src/screens/WebNavigation.tsx
@@ -33,17 +33,17 @@ const WebNavigation = () => {
 
     return (
         <div className="flex flex-col justify-center items-center h-full">
-            <Grid container justifyContent='flex-start' alignItems='center' spacing={(isXl || isLg) ? 8 : (isMd) ? 1 : 2} className='h-full'>
-                <Grid container item spacing={(isXl || isLg) ? 4 : (isMd) ? 1 : 2} xs={12}>
-                    <Grid container item spacing={(isXl || isLg) ? 1 : 0} xs={12}>
+            <Grid container justifyContent='flex-start' alignItems='center' rowGap={isXl ? 10 : isLg ? 4 : isMd ? 1 : 2} className='h-full'>
+                <Grid container item rowGap={isXl ? 6 : isLg ? 4 : isMd ? 2 : 1} xs={12}>
+                    <Grid container item rowGap={(isXl || isLg) ? 1 : 0} xs={12}>
                         <Grid item xs={12}>
-                            <Typography variant="h2" fontWeight={600} fontFamily='inter'>
+                            <Typography variant={isMd ? "h4" : "h2"} fontWeight={600} fontFamily='inter'>
                                 <span className='easter-egg' onClick={() => adminPanelNavigation()}>N</span>a
                                 <span className='easter-egg' onClick={() => adminPanelNavigation()}>d</span>eem Abdun
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <Typography variant="h5" fontWeight={500} fontFamily='inter'>
+                            <Typography variant={isXl ? "h4" : isMd ? "h6" : "h5"} fontWeight={500} fontFamily='inter'>
                                 <Typewriter
                                     options={{
                                         strings: ["Web App Developer", "Mobile App Developer", "Mechanical Engineer"],
@@ -55,42 +55,42 @@ const WebNavigation = () => {
                         </Grid>
                     </Grid>
                     <Grid item xs={12}>
-                        <Typography variant="body1" fontWeight={400} fontFamily='inter'>Transforming concepts into code, Specialized in delivering pixel-perfect, accessible wonders as a full-stack web and app developer.</Typography>
+                        <Typography variant={isXl ? "h5" : isMd ? "body2" : "body1"} fontWeight={400} fontFamily='inter'>Transforming concepts into code, Specialized in delivering pixel-perfect, accessible wonders as a full-stack web and app developer.</Typography>
                     </Grid>
-                    <Grid container item spacing={(isXl || isLg) ? 2 : 1} xs={12}>
+                    <Grid container item rowGap={(isXl || isLg) ? 2 : 1} xs={12}>
                         <Grid item xs={12}>
-                            <Typography variant="h5" fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section0')}>
+                            <Typography variant={isXl ? "h4" : isMd ? "h6" : "h5"} fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section0')}>
                                 <pre>&lt;About Me /&gt;</pre>
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <Typography variant="h5" fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section1')}>
+                            <Typography variant={isXl ? "h4" : isMd ? "h6" : "h5"} fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section1')}>
                                 <pre>&lt;Experience /&gt;</pre>
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <Typography variant="h5" fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section2')}>
+                            <Typography variant={isXl ? "h4" : isMd ? "h6" : "h5"} fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section2')}>
                                 <pre>&lt;Wall of Code /&gt;</pre>
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <Typography variant="h5" fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section3')}>
+                            <Typography variant={isXl ? "h4" : isMd ? "h6" : "h5"} fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section3')}>
                                 <pre>&lt;Projects /&gt;</pre>
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <Typography variant="h5" fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section4')}>
+                            <Typography variant={isXl ? "h4" : isMd ? "h6" : "h5"} fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section4')}>
                                 <pre>&lt;Contact Me /&gt;</pre>
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <Typography variant="h5" fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section5')}>
+                            <Typography variant={isXl ? "h4" : isMd ? "h6" : "h5"} fontWeight={400} fontFamily='inter' className="underline-hover" onClick={() => scrollToSection('section5')}>
                                 <pre>&lt;Summary /&gt;</pre>
                             </Typography>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid container item spacing={(isXl || isLg) ? 2 : 1} xs={12}>
+                <Grid container item justifyContent="space-evenly" alignItems="center" xs={12}>
                     <Grid item>
                         <IconButton className="icon-btn" aria-label="GitHub" href='https://github.com/Nadeem-Abdun' target="_blank"><GithubIcon /></IconButton>
                     </Grid>
