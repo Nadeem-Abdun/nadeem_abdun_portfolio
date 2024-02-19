a3320a2fe6a0c8a3f23bcde66ea0ac8f33252637
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-19 15:09:25 +0530

    Initiated AboutMe Card In Admin Home

---
+++ src/components/AdminComponents/AboutMeCard.tsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import { Grid, Typography, Link, Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from '@mui/material';
+import '../ComponentStyles.css';
+import { useMediaQuery } from 'react-responsive';
+import ProfilePic from '../../assets/images/Profile_Avatar.png';
+
+const AboutMeCard = () => {
+
+    const isXl = useMediaQuery({ query: '(min-width: 1920px)' });
+    const isLg = useMediaQuery({ query: '(min-width: 1280px) and (max-width: 1919px)' });
+    const isMd = useMediaQuery({ query: '(min-width: 960px) and (max-width: 1279px)' });
+    const isSm = useMediaQuery({ query: '(min-width: 600px) and (max-width: 959px)' });
+    const isXs = useMediaQuery({ query: '(min-width: 320px) and (max-width: 599px)' });
+
+    const [profilePicDialog, setProfilePicDialog] = useState(false);
+
+    const handleProfilePicDialog = () => {
+        setProfilePicDialog(!profilePicDialog);
+    }
+
+    const [profileDescDialog, setprofileDescDialog] = useState(false);
+
+    const handleProfileDescDialog = () => {
+        setprofileDescDialog(!profileDescDialog);
+    }
+
+    return (
+        <div className={`admin-card ${(isXs) ? 'px-2' : 'px-4'} ${(isXs) ? 'py-2' : 'py-4'} w-full`}>
+            <Grid container justifyContent="center" alignItems="center" rowGap={3}>
+
+                <Grid item xs={12}>
+                    <Typography variant="h5" fontWeight={500} fontFamily='inter'>About Me</Typography>
+                </Grid>
+
+                <Grid container item xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={1}>
+                    <Grid item>
+                        <img
+                            src={ProfilePic}
+                            alt='Profile_Picture'
+                            style={{
+                                width: isXs ? '45vw' : isSm ? '35vw' : '16vw',
+                                height: isXs ? '45vw' : isSm ? '35vw' : '16vw',
+                                borderRadius: '20%',
+                            }}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Link variant="caption" color="#00ffff" className="hover:text-pink-300" onClick={() => handleProfilePicDialog()}>
+                            Click Here To Update Profile Picture
+                        </Link>
+                    </Grid>
+                </Grid>
+
+                <Grid container item xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={1}>
+                    <Grid item>
+                        <Typography variant="body1" fontWeight={400} fontFamily='inter'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro....</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="body1" fontWeight={400} fontFamily='inter'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro....</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Link variant="caption" color="#00ffff" className="hover:text-pink-300" onClick={() => handleProfileDescDialog()}>
+                            Click Here To Update Description
+                        </Link>
+                    </Grid>
+                </Grid>
+
+            </Grid>
+            <Dialog open={profilePicDialog} onClose={() => handleProfilePicDialog()} disableEscapeKeyDown maxWidth="lg" fullScreen={isXs}>
+                <DialogTitle>
+                    <Typography variant="h6" fontWeight={500} fontFamily='inter'>Update Profile Picture</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={2}>
+                        <Grid item>
+                            <img
+                                src={ProfilePic}
+                                alt='Profile_Picture'
+                                style={{
+                                    width: isXs ? '75vw' : isSm ? '65vw' : '12vw',
+                                    height: isXs ? '75vw' : isSm ? '65vw' : '12vw',
+                                    borderRadius: '20%',
+                                }}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button variant="text" component="label">
+                                Upload Image
+                                <input type="file" hidden />
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Grid container xs={12} justifyContent="flex-end" alignItems="center" columnGap={1}>
+                        <Grid item>
+                            <Button variant="contained" color="error" onClick={() => handleProfilePicDialog()}>Close</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained" color="success" onClick={() => handleProfilePicDialog()}>Update</Button>
+                        </Grid>
+                    </Grid>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={profileDescDialog} onClose={() => handleProfileDescDialog()} disableEscapeKeyDown maxWidth="lg" fullScreen={isXs}>
+                <DialogTitle>
+                    <Typography variant="h6" fontWeight={500} fontFamily='inter'>Update Profile Description</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={2} className="mt-2">
+                        <Grid item>
+                            <TextField label="Description 1" variant="outlined" multiline value="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro...." />
+                        </Grid>
+                        <Grid item>
+                            <TextField label="Description 2" variant="outlined" multiline value="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro...." />
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Grid container xs={12} justifyContent="flex-end" alignItems="center" columnGap={1}>
+                        <Grid item>
+                            <Button variant="contained" color="error" onClick={() => handleProfileDescDialog()}>Close</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained" color="success" onClick={() => handleProfileDescDialog()}>Update</Button>
+                        </Grid>
+                    </Grid>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
+
+export default AboutMeCard
