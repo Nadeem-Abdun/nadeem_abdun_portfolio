eb4e517273772136642464a15e522680a24fdead
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-20 19:57:48 +0530

    Added onChange Attribute for TextFields

--- src/components/AdminComponents/AboutMeCard.tsx
+++ src/components/AdminComponents/AboutMeCard.tsx
@@ -24,14 +24,23 @@ const AboutMeCard = () => {
         setprofileDescDialog(!profileDescDialog);
     }
 
+    const [profileDesc, setProfileDesc] = useState({
+        desc1: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro....",
+        desc2: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro...."
+    });
+
+    const handleProfileDescChange = (name: string, value: string) => {
+        setProfileDesc({
+            ...profileDesc, [name]: value
+        });
+    }
+
     return (
         <div className={`admin-card ${(isXs) ? 'px-2' : 'px-4'} ${(isXs) ? 'py-2' : 'py-4'} w-full`}>
             <Grid container justifyContent="center" alignItems="center" rowGap={3}>
-
                 <Grid item xs={12}>
                     <Typography variant="h5" fontWeight={500} fontFamily='inter'>About Me</Typography>
                 </Grid>
-
                 <Grid container item xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={1}>
                     <Grid item>
                         <img
@@ -50,13 +59,16 @@ const AboutMeCard = () => {
                         </Link>
                     </Grid>
                 </Grid>
-
                 <Grid container item xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={1}>
                     <Grid item>
-                        <Typography variant="body1" fontWeight={400} fontFamily='inter'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro....</Typography>
+                        <Typography variant="body1" fontWeight={400} fontFamily='inter'>
+                            {profileDesc.desc1}
+                        </Typography>
                     </Grid>
                     <Grid item>
-                        <Typography variant="body1" fontWeight={400} fontFamily='inter'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro....</Typography>
+                        <Typography variant="body1" fontWeight={400} fontFamily='inter'>
+                            {profileDesc.desc2}
+                        </Typography>
                     </Grid>
                     <Grid item>
                         <Link variant="caption" color="#00ffff" className="hover:text-pink-300" onClick={() => handleProfileDescDialog()}>
@@ -64,14 +76,13 @@ const AboutMeCard = () => {
                         </Link>
                     </Grid>
                 </Grid>
-
             </Grid>
-            <Dialog open={profilePicDialog} onClose={() => handleProfilePicDialog()} disableEscapeKeyDown maxWidth="lg" fullScreen={isXs}>
+            <Dialog open={profilePicDialog} onClose={() => handleProfilePicDialog()} disableEscapeKeyDown fullScreen={isXs}>
                 <DialogTitle>
                     <Typography variant="h6" fontWeight={500} fontFamily='inter'>Update Profile Picture</Typography>
                 </DialogTitle>
                 <DialogContent>
-                    <Grid container xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={2}>
+                    <Grid container xs={12} justifyContent="center" alignItems="center" rowGap={2}>
                         <Grid item>
                             <img
                                 src={ProfilePic}
@@ -83,8 +94,8 @@ const AboutMeCard = () => {
                                 }}
                             />
                         </Grid>
-                        <Grid item>
-                            <Button variant="text" component="label">
+                        <Grid item xs={12}>
+                            <Button variant="text" fullWidth>
                                 Upload Image
                                 <input type="file" hidden />
                             </Button>
@@ -102,18 +113,35 @@ const AboutMeCard = () => {
                     </Grid>
                 </DialogActions>
             </Dialog>
-
-            <Dialog open={profileDescDialog} onClose={() => handleProfileDescDialog()} disableEscapeKeyDown maxWidth="lg" fullScreen={isXs}>
+            <Dialog open={profileDescDialog} onClose={() => handleProfileDescDialog()} disableEscapeKeyDown fullScreen={isXs}>
                 <DialogTitle>
                     <Typography variant="h6" fontWeight={500} fontFamily='inter'>Update Profile Description</Typography>
                 </DialogTitle>
                 <DialogContent>
-                    <Grid container xs={12} justifyContent="center" alignItems="center" flexDirection="column" rowGap={2} className="mt-2">
-                        <Grid item>
-                            <TextField label="Description 1" variant="outlined" multiline value="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro...." />
+                    <Grid container xs={12} justifyContent="flex-start" alignItems="center" rowGap={2} className="mt-2">
+                        <Grid item xs={12}>
+                            <TextField
+                                id="AboutMe-Description-Field-1"
+                                label="Description 1"
+                                variant="filled"
+                                type="text"
+                                multiline
+                                fullWidth
+                                value={profileDesc.desc1}
+                                onChange={(event) => handleProfileDescChange("desc1", event.target.value)}
+                            />
                         </Grid>
-                        <Grid item>
-                            <TextField label="Description 2" variant="outlined" multiline value="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore accusantium itaque culpa nam porro...." />
+                        <Grid item xs={12}>
+                            <TextField
+                                id="AboutMe-Description-Field-2"
+                                label="Description 2"
+                                variant="filled"
+                                type="text"
+                                multiline
+                                fullWidth
+                                value={profileDesc.desc2}
+                                onChange={(event) => handleProfileDescChange("desc2", event.target.value)}
+                            />
                         </Grid>
                     </Grid>
                 </DialogContent>
