abc94548b94598f41e64dcb251a55ababfa36884
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-20 20:02:31 +0530

    Fixed Cards Alignment

--- src/screens/AdminScreens/AdminHome.tsx
+++ src/screens/AdminScreens/AdminHome.tsx
@@ -21,15 +21,15 @@ const AdminHome = () => {
     }, []);
 
     return (
-        <Grid container justifyContent='center' alignItems='center' flexDirection='column' rowGap={4} wrap='nowrap'>
-            <Grid item xs={12} className='text-center'>
-                <Typography variant="h4" fontWeight={500} fontFamily='inter'>Admin Home</Typography>
+        <Grid container justifyContent="center" alignItems="center" flexDirection="column" rowGap={4} wrap="nowrap">
+            <Grid item xs={12} className="text-center">
+                <Typography variant="h4" fontWeight={500} fontFamily="inter">Admin Home</Typography>
             </Grid>
-            <Grid container item xl={10} lg={10} md={11} sm={11} xs={11} justifyContent='center' alignItems='center' rowGap={2}>
+            <Grid container item xl={10} lg={10} md={11} sm={11} xs={11} justifyContent="center" alignItems="center" rowGap={2}>
                 <Grid item xs={12}>
                     <AnalyticsCard />
                 </Grid>
-                <Grid container item xs={12} justifyContent='center' alignItems='center' rowGap={2} columnSpacing={2}>
+                <Grid container item xs={12} justifyContent="center" alignItems="flex-start" rowGap={2} columnSpacing={2}>
                     <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
                         <AboutMeCard />
                     </Grid>
@@ -47,7 +47,7 @@ const AdminHome = () => {
                     </Grid>
                     <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
                         <AboutMeCard />
-                    </Grid>                    
+                    </Grid>
                 </Grid>
             </Grid>
         </Grid>
