25b0ddfaec78e190f75ae29fb822deca28cc8e25
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-03-03 11:25:15 +0530

    Code Refactoring Done

--- src/components/AdminComponents/ExperienceCard.tsx
+++ src/components/AdminComponents/ExperienceCard.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import { Grid, Typography, Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, IconButton } from "@mui/material";
-import "../ComponentStyles.css";
-import { useMediaQuery } from "react-responsive";
+import { useBreakpoints } from "../../utils/Breakpoints";
 import { Add } from "@mui/icons-material";
+import "../../styles/componentStyles.css";
 
 const ExperienceCard = () => {
 
-    const isXl = useMediaQuery({ query: "(min-width: 1920px)" });
-    const isLg = useMediaQuery({ query: "(min-width: 1280px) and (max-width: 1919px)" });
-    const isMd = useMediaQuery({ query: "(min-width: 960px) and (max-width: 1279px)" });
-    const isSm = useMediaQuery({ query: "(min-width: 600px) and (max-width: 959px)" });
-    const isXs = useMediaQuery({ query: "(min-width: 320px) and (max-width: 599px)" });
+    const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
 
     const experienceList = [
         {
