eba1bf7083083192e15076cb47eaf699d657e825
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-03-03 11:26:10 +0530

    Code Refactoring Done

--- src/components/AdminComponents/AboutMeCard.tsx
+++ src/components/AdminComponents/AboutMeCard.tsx
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
-import { Grid, Typography, Link, Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from '@mui/material';
-import '../ComponentStyles.css';
-import { useMediaQuery } from 'react-responsive';
-import ProfilePic from '../../assets/images/Profile_Avatar.png';
+import React, { useState } from "react";
+import { Grid, Typography, Link, Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from "@mui/material";
+import { useBreakpoints } from "../../utils/Breakpoints";
+import ProfilePic from "../../assets/images/Profile_Avatar.png";
+import "../../styles/componentStyles.css";
 
 const AboutMeCard = () => {
 
-    const isXl = useMediaQuery({ query: '(min-width: 1920px)' });
-    const isLg = useMediaQuery({ query: '(min-width: 1280px) and (max-width: 1919px)' });
-    const isMd = useMediaQuery({ query: '(min-width: 960px) and (max-width: 1279px)' });
-    const isSm = useMediaQuery({ query: '(min-width: 600px) and (max-width: 959px)' });
-    const isXs = useMediaQuery({ query: '(min-width: 320px) and (max-width: 599px)' });
+    const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
 
     const [profilePicDialog, setProfilePicDialog] = useState(false);
 
