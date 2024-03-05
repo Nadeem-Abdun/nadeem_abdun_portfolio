1052f0401e5278b6b73e66bda0d548a8efe8e1ca
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-03-05 22:50:14 +0530

    Code Refactoring Done

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './styles/tailwind.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import "./styles/tailwind.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
