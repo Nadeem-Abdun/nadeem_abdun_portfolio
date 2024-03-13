06c36289532c48120e6e8fab339e6e999760ce3f
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-03-13 05:32:02 +0530

    Removed An Easter Egg

--- src/screens/AppScreens/MobileNavigation.tsx
+++ src/screens/AppScreens/MobileNavigation.tsx
@@ -34,8 +34,7 @@ const MobileNavigation = () => {
                     <Grid container item rowGap={2} xs={12}>
                         <Grid item xs={12}>
                             <Typography variant="h2" fontWeight={600} fontFamily='inter'>
-                                <span className='easter-egg' onClick={() => adminPanelNavigation()}>N</span>a
-                                <span className='easter-egg' onClick={() => adminPanelNavigation()}>d</span>eem Abdun
+                                <span className='easter-egg' onClick={() => adminPanelNavigation()}>N</span>adeem Abdun
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
