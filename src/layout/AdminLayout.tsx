012a77b333fa699f88896593de56142e3b58ee4c
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-03-18 11:27:07 +0530

    Code Refactoring Done

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Grid } from '@mui/material';
-import { useMediaQuery } from 'react-responsive';
-import ScrollToTop from '../components/ScrollToTop';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
+import React from "react";
+import { Grid } from "@mui/material";
+import { useBreakpoints } from "../utils/Breakpoints";
+import ScrollToTop from "../components/ScrollToTop";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 
 interface Props {
     childElement?: React.ReactNode;
@@ -14,11 +14,7 @@ const AdminLayout: React.FC<Props> = (props) => {
 
     const { childElement } = props;
 
-    const isXl = useMediaQuery({ query: '(min-width: 1920px)' });
-    const isLg = useMediaQuery({ query: '(min-width: 1280px) and (max-width: 1919px)' });
-    const isMd = useMediaQuery({ query: '(min-width: 960px) and (max-width: 1279px)' });
-    const isSm = useMediaQuery({ query: '(min-width: 600px) and (max-width: 959px)' });
-    const isXs = useMediaQuery({ query: '(min-width: 320px) and (max-width: 599px)' });
+    const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
 
     return (
         <Grid container>
