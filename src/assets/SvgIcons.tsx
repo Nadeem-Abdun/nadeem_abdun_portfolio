a6af8f75e00553a2e4f3e61264c9eab7e1cdabf3
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-03-22 13:38:28 +0530

    New SVG Icon Added

--- src/assets/SvgIcons.tsx
+++ src/assets/SvgIcons.tsx
@@ -88,6 +88,20 @@ export const DiscordIcon: React.FC<SvgIconProps> = ({ color = '#D1D5DB' }) => {
     )
 }
 
+export const MailIcon: React.FC<SvgIconProps> = ({ color = '#D1D5DB' }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            fill={color}
+            className="h-8 w-8 transition-colors duration-300 ease-in-out"
+            aria-hidden="true"
+        >
+            <path d="M29 4H3a3 3 0 0 0-3 3v18a3 3 0 0 0 3 3h26a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-.72 2L16 14.77 3.72 6zM30 25a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.23l13.42 9.58a1 1 0 0 0 1.16 0L30 7.23z" />
+        </svg>
+    )
+}
+
 export const HtmlIcon: React.FC<SvgIconProps> = () => {
     return (
         <svg
