b2be9f749d57cdecbf8b8a67425c95507eba1354
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-04-30 11:22:14 +0530

    Date Formatter Component Improved

--- src/utils/DateFormatter.ts
+++ src/utils/DateFormatter.ts
@@ -2,14 +2,25 @@ interface DateOptionType {
     month?: "numeric" | "2-digit" | "short" | "long" | "narrow",
     year?: "numeric" | "2-digit",
 }
-
-// interface InputDateType {
-//     date?: Date | number | undefined,
-// }
-
-const formatDate = (date: any) => {
+export const formatToLocaleString = (date: any) => {
+    date = new Date(date);
     const options: DateOptionType = { month: "short", year: "numeric" };
     return new Intl.DateTimeFormat("en-US", options).format(date);
 };
 
-export default formatDate;
+export const formatToStringYYYYMMDD = (date: any) => {
+    if (date === "") {
+        return new Date();
+    }
+    date = new Date(date);
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (day < 10) {
+        day = '0' + day;
+    }
+    return `${year}-${month}-${day}`;
+};
