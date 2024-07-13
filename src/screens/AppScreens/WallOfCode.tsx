caa543cb44ba0851f6fd569f484173bff7e475a2
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-07-13 12:44:48 +0530

    Added Redux State Management For WallOfCode

--- src/screens/AppScreens/WallOfCode.tsx
+++ src/screens/AppScreens/WallOfCode.tsx
@@ -1,72 +1,17 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
 import { useBreakpoints } from "../../utils/Breakpoints";
-import { HtmlIcon, CssIcon, JavascriptIcon, TypescriptIcon, ReactIcon, ReactNativeIcon, JavaIcon, NodeJsIcon, ExpressJsIcon, MaterialUiIcon, MongoDbIcon, MySqlIcon, TailwindCssIcon, ReduxIcon } from "../../assets/SvgIcons";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 import SkillCard from "../../components/AppComponents/SkillCard";
 import "../../styles/screenStyles.css";
 
 const WallOfCode = () => {
-
+    
     const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
 
-    const skillData = [
-        {
-            icon: <HtmlIcon aria-label="Html" />,
-            name: "HTML"
-        },
-        {
-            icon: <CssIcon aria-label="Css" />,
-            name: "CSS"
-        },
-        {
-            icon: <JavascriptIcon aria-label="JavaScript" />,
-            name: "JavaScript"
-        },
-        {
-            icon: <TypescriptIcon aria-label="TypeScript" />,
-            name: "TypeScript"
-        },
-        {
-            icon: <ReactIcon aria-label="React_JS" />,
-            name: "ReactJS"
-        },
-        {
-            icon: <ReactNativeIcon aria-label="React_Native" />,
-            name: "React Native"
-        },
-        {
-            icon: <JavaIcon aria-label="Java" />,
-            name: "Java"
-        },
-        {
-            icon: <NodeJsIcon aria-label="Node_Js" />,
-            name: "Node Js"
-        },
-        {
-            icon: <ExpressJsIcon aria-label="Express_Js" />,
-            name: "Express Js"
-        },
-        {
-            icon: <MaterialUiIcon aria-label="Material_UI" />,
-            name: "Material UI"
-        },
-        {
-            icon: <MongoDbIcon aria-label="Mongo_Db" />,
-            name: "MongoDb"
-        },
-        {
-            icon: <MySqlIcon aria-label="My_SQL" />,
-            name: "MySQL"
-        },
-        {
-            icon: <TailwindCssIcon aria-label="Tailwind_Css" />,
-            name: "Tailwind Css"
-        },
-        {
-            icon: <ReduxIcon aria-label="Redux_Toolkit" />,
-            name: "Redux Toolkit"
-        },
-    ];
+    const { wallOfCode } = useSelector((state: RootState) => state.wallOfCode);
+    const dispatch = useDispatch;
 
     return (
         <div id='section-wallofcode' className={`flex flex-col justify-center items-center ${(isXs || isSm || isMd) ? 'my-3' : 'my-10'}`}>
@@ -75,11 +20,11 @@ const WallOfCode = () => {
                     <Typography variant="h4" fontWeight={500} fontFamily='inter'>Wall of Code</Typography>
                 </Grid>
                 <Grid container xs={12} justifyContent='flex-start' alignItems='center' spacing={1}>
-                    {skillData && skillData.map((skill, index) => {
-                        const { icon, name } = skill
+                    {wallOfCode && wallOfCode.map((skill, index) => {
+                        const { skillName, skillIcon } = skill
                         return (
                             <Grid key={index} item xl={3} lg={3} md={4} sm={6} xs={6}>
-                                <SkillCard icon={icon} name={name} />
+                                <SkillCard skillName={skillName} skillIcon={skillIcon} />
                             </Grid>
                         )
                     })
