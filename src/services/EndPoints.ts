553319a9e84fb29a4518838e693609b613624e5c
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-07-23 20:47:04 +0530

    Added Profile And ContactMe API Paths

--- src/services/EndPoints.ts
+++ src/services/EndPoints.ts
@@ -1,7 +1,7 @@
 const serviceUrls = {
     // baseUrl: "http://localhost:4000",
     baseUrl: "https://nadeem-abdun-portfolio-backend.onrender.com",
-}
+};
 
 export const userPaths = {
     SignUp: `${serviceUrls.baseUrl}/api/v1/users/register`,
@@ -11,5 +11,19 @@ export const userPaths = {
     UpdatePassword: `${serviceUrls.baseUrl}/api/v1/users/update-password`,
     GetUserDetails: `${serviceUrls.baseUrl}/api/v1/users/get-user`,
     UpdateUserDetails: `${serviceUrls.baseUrl}/api/v1/users/update-account`,
-    GetUserProfiles: `${serviceUrls.baseUrl}/api/v1/users/get-profile/:username`,
-}
+    GetUserProfiles: (username: string) => `${serviceUrls.baseUrl}/api/v1/users/get-profile/${username}`,
+};
+
+export const profilePaths = {
+    CreateProfile: `${serviceUrls.baseUrl}/api/v1/profile/create`,
+    GetProfile: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/profile/get/${profileId}`,
+    UpdateProfile: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/profile/update/${profileId}`,
+    DeleteProfile: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/profile/delete/${profileId}`,
+};
+
+export const contactMePaths = {
+    CreateContactForm: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/create/${profileId}`,
+    GetContactForms: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/getForms/${profileId}`,
+    ReplyContactForm: (id: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/reply/${id}`,
+    DeleteContactForm: (id: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/delete/${id}`,
+};
