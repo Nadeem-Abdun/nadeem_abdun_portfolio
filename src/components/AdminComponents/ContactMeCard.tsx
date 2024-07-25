47ef1652d442cc47f0ef504bbbc55093e4ba7c89
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-07-25 15:37:10 +0530

    Redux State Management And Api Integration

--- src/components/AdminComponents/ContactMeCard.tsx
+++ src/components/AdminComponents/ContactMeCard.tsx
@@ -1,52 +1,147 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Typography, Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, IconButton } from "@mui/material";
 import { DeleteForeverOutlined } from "@mui/icons-material";
 import { useBreakpoints } from "../../utils/Breakpoints";
-import { ContactMe, ContactMeState } from "../../redux/contactMe/contactMeSlice";
+import { ContactMe, ContactMeState, deleteContactFormFailure, deleteContactFormSuccess, getContactFormFailure, getContactFormSuccess, replyContactFormFailure, replyContactFormSuccess, resetDeleteContactForm, resetGetContactForm, resetReplyContactForm, submitDeleteContactForm, submitGetContactForm, submitReplyContactForm } from "../../redux/contactMe/contactMeSlice";
+import { DeleteContactForm, GetContactForm, PatchReplyContactForm } from "../../services/ServiceControllers";
 import ellipsisString from "../../utils/EllipsisString";
 import "../../styles/componentStyles.css";
+import { useDispatch } from "react-redux";
 
-const ContactMeCard: React.FC<ContactMeState> = (props) => {
-    const { contactMeForms } = props;
+interface Props extends ContactMeState {
+    profile: string[] | undefined;
+    handleAlertSliderOpen: (type: string, message: string) => void
+}
 
+const ContactMeCard: React.FC<Props> = (props) => {
+    const { profile, contactMeForms, handleAlertSliderOpen } = props;
     const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
+    const dispatch = useDispatch();
 
+    // Local State Management
     const [selectedContactRequest, setSelectedContactRequest] = useState<ContactMe>({
+        _id: "",
         visitorName: "",
         visitorEmail: "",
         visitorPhone: "",
         visitorMessage: "",
+        userReplyMessage: "",
     });
-    const [requestReply, setrequestReply] = useState("");
     const [contactRequestDialog, setContactRequestDialog] = useState(false);
     const [dialogType, setDialogType] = useState("view");
 
+    // Api Calls
+    const getContactFormsApiCall = async () => {
+        try {
+            const profileId = (profile && profile.length !== 0) ? profile[0] : '';
+            const response = await GetContactForm(profileId);
+            if (response.success === true) {
+                handleAlertSliderOpen("success", response.message);
+            } else {
+                handleAlertSliderOpen("error", response.message);
+            }
+            return response;
+        } catch (error) {
+            console.error("Unexpected error: " + error);
+            handleAlertSliderOpen("error", "Unexpected error encountered");
+        }
+    };
+    const patchReplyContactApiCall = async () => {
+        try {
+            const formData = {
+                userReplyMessage: selectedContactRequest.userReplyMessage,
+            };
+            const contactFormId = selectedContactRequest._id || "";
+            const response = await PatchReplyContactForm(formData, contactFormId);
+            if (response.success === true) {
+                handleAlertSliderOpen("success", response.message);
+            } else {
+                handleAlertSliderOpen("error", response.message);
+            }
+            return response;
+        } catch (error) {
+            console.error("Unexpected error: " + error);
+            handleAlertSliderOpen("error", "Unexpected error encountered");
+        }
+    };
+    const deleteContactFormApiCall = async () => {
+        try {
+            const contactFormId = selectedContactRequest._id || "";
+            const response = await DeleteContactForm(contactFormId);
+            if (response.success === true) {
+                handleAlertSliderOpen("success", response.message);
+            } else {
+                handleAlertSliderOpen("error", response.message);
+            }
+            return response;
+        } catch (error) {
+            console.error("Unexpected error: " + error);
+            handleAlertSliderOpen("error", "Unexpected error encountered");
+        }
+    };
+
+    // Contact Form Submit Functions
+    const handleGetContactFormsSubmit = async () => {
+        dispatch(submitGetContactForm());
+        const response = await getContactFormsApiCall();
+        if (response.success) {
+            const contactFormsData = response?.data;
+            dispatch(getContactFormSuccess(contactFormsData));
+            dispatch(resetGetContactForm());
+        } else {
+            dispatch(getContactFormFailure());
+        }
+    };
+    const handleReplyContactFormSubmit = async () => {
+        dispatch(submitReplyContactForm());
+        const response = await patchReplyContactApiCall();
+        if (response.success) {
+            const contactFormData = response?.data;
+            dispatch(replyContactFormSuccess(contactFormData));
+            dispatch(resetReplyContactForm());
+            handleContactDialogClose();
+        } else {
+            dispatch(replyContactFormFailure());
+        }
+    };
+    const handleDeleteContactFormSubmit = async () => {
+        dispatch(submitDeleteContactForm());
+        const response = await deleteContactFormApiCall();
+        if (response.success) {
+            const contactFormData = response?.data;
+            dispatch(deleteContactFormSuccess(contactFormData));
+            dispatch(resetDeleteContactForm());
+            handleContactDialogClose();
+        } else {
+            dispatch(deleteContactFormFailure());
+        }
+    };
+
+    // Dialog Functions
     const handleContactDialogOpen = (type: string, item?: ContactMe) => {
         setContactRequestDialog(true);
         setDialogType(type);
         if (item) {
             setSelectedContactRequest(item);
         }
-    }
-
+    };
     const handleContactDialogClose = () => {
         setContactRequestDialog(false);
         setSelectedContactRequest({
+            _id: "",
             visitorName: "",
             visitorEmail: "",
             visitorPhone: "",
             visitorMessage: "",
+            userReplyMessage: "",
         })
-    }
+    };
 
-    const handleContactReply = () => {
-        setContactRequestDialog(false);
-    }
-
-    const handleRequestDelete = () => {
-        setContactRequestDialog(false);
-    }
-
+    useEffect(() => {
+        if (profile && profile.length !== 0) {
+            handleGetContactFormsSubmit();
+        }
+    }, []);
     return (
         <div className={`admin-card ${(isXs) ? "px-2" : "px-4"} ${(isXs) ? "py-2" : "py-4"} relative min-h-96 max-h-96 overflow-auto`}>
             <Grid container xs={12} justifyContent="center" alignItems="center" rowGap={2}>
@@ -70,16 +165,24 @@ const ContactMeCard: React.FC<ContactMeState> = (props) => {
                                 <Grid container item xs={12} justifyContent="space-between" alignItems="flex-start">
                                     <Grid container item xs={10} direction="column">
                                         <Grid item>
-                                            <Typography className="text-cyan-400" variant="body1" fontWeight={500} fontFamily='inter'>Visitor Name:-</Typography>
+                                            <Typography className="text-cyan-400" variant="body1" fontWeight={500} fontFamily='inter'>Contact Id:-</Typography>
                                         </Grid>
                                         <Grid item>
-                                            <Typography variant="body2" fontWeight={400} fontFamily='inter'>{ellipsisString(item.visitorName, 25)}</Typography>
+                                            <Typography variant="body2" fontWeight={400} fontFamily='inter'>{item._id}</Typography>
                                         </Grid>
                                     </Grid>
                                     <Grid item>
                                         <IconButton onClick={(e) => { e.stopPropagation(); handleContactDialogOpen("delete", item) }}>
                                             <DeleteForeverOutlined className="text-cyan-400 hover:text-cyan-200" />
                                         </IconButton>
+                                    </Grid>
+                                </Grid>
+                                <Grid container item xs={12} direction="column">
+                                    <Grid item>
+                                        <Typography className="text-cyan-400" variant="body1" fontWeight={500} fontFamily='inter'>Visitor Name:-</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="body2" fontWeight={400} fontFamily='inter'>{ellipsisString(item.visitorName, 25)}</Typography>
                                     </Grid>
                                 </Grid>
                                 <Grid container item xs={12} direction="column">
@@ -106,6 +209,16 @@ const ContactMeCard: React.FC<ContactMeState> = (props) => {
                                         <Typography variant="body2" fontWeight={400} fontFamily='inter'>{ellipsisString(item.visitorPhone, 25)}</Typography>
                                     </Grid>
                                 </Grid>
+                                {item.userReplyMessage &&
+                                    <Grid container item xs={12} direction="column">
+                                        <Grid item>
+                                            <Typography className="text-cyan-400" variant="body1" fontWeight={500} fontFamily='inter'>User Reply:-</Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="body2" fontWeight={400} fontFamily='inter'>{ellipsisString(item.userReplyMessage, 25)}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                }
                             </Grid>
                         )
                     })}
@@ -146,12 +259,12 @@ const ContactMeCard: React.FC<ContactMeState> = (props) => {
                                     <Typography variant="body1" fontWeight={500} fontFamily="inter">Reply to request:</Typography>
                                     <TextField
                                         id="contactme_request_reply_field"
-                                        variant="filled"
+                                        variant="standard"
                                         type="text"
                                         multiline
                                         fullWidth
-                                        value={requestReply}
-                                        onChange={(event) => setrequestReply(event.target.value)}
+                                        value={selectedContactRequest.userReplyMessage}
+                                        onChange={(event) => setSelectedContactRequest({ ...selectedContactRequest, userReplyMessage: event.target.value })}
                                     />
                                 </Grid>
                             </>
@@ -169,11 +282,11 @@ const ContactMeCard: React.FC<ContactMeState> = (props) => {
                         </Grid>
                         {(dialogType === "view") ?
                             <Grid item>
-                                <Button variant="contained" color="success" onClick={() => handleContactReply()}>Send Reply</Button>
+                                <Button variant="contained" color="success" onClick={() => handleReplyContactFormSubmit()}>Send Reply</Button>
                             </Grid>
                             :
                             <Grid item>
-                                <Button variant="contained" color="error" onClick={() => handleRequestDelete()}>Delete Request</Button>
+                                <Button variant="contained" color="error" onClick={() => handleDeleteContactFormSubmit()}>Delete Request</Button>
                             </Grid>
                         }
                     </Grid>
