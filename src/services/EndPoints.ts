a3a6f6bdceeb7332589f3db01de30641d68b34c6
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-07-31 16:49:39 +0530

    Added Api Paths For Experience And Minor Changes

--- src/services/EndPoints.ts
+++ src/services/EndPoints.ts
@@ -2,28 +2,37 @@ const serviceUrls = {
     // baseUrl: "http://localhost:4000",
     baseUrl: "https://nadeem-abdun-portfolio-backend.onrender.com",
 };
+const apiVersionV1 = '/api/v1';
 
 export const userPaths = {
-    SignUp: `${serviceUrls.baseUrl}/api/v1/users/register`,
-    Login: `${serviceUrls.baseUrl}/api/v1/users/login`,
-    Logout: `${serviceUrls.baseUrl}/api/v1/users/logout`,
-    RefreshToken: `${serviceUrls.baseUrl}/api/v1/users/refresh-token`,
-    UpdatePassword: `${serviceUrls.baseUrl}/api/v1/users/update-password`,
-    GetUserDetails: `${serviceUrls.baseUrl}/api/v1/users/get-user`,
-    UpdateUserDetails: `${serviceUrls.baseUrl}/api/v1/users/update-account`,
-    GetUserProfiles: (username: string) => `${serviceUrls.baseUrl}/api/v1/users/get-profile/${username}`,
+    SignUp: `${serviceUrls.baseUrl}${apiVersionV1}/users/register`,
+    Login: `${serviceUrls.baseUrl}${apiVersionV1}/users/login`,
+    Logout: `${serviceUrls.baseUrl}${apiVersionV1}/users/logout`,
+    RefreshToken: `${serviceUrls.baseUrl}${apiVersionV1}/users/refresh-token`,
+    UpdatePassword: `${serviceUrls.baseUrl}${apiVersionV1}/users/update-password`,
+    GetUserDetails: `${serviceUrls.baseUrl}${apiVersionV1}/users/get-user`,
+    UpdateUserDetails: `${serviceUrls.baseUrl}${apiVersionV1}/users/update-account`,
+    GetUserProfiles: (username: string) => `${serviceUrls.baseUrl}${apiVersionV1}/users/get-profile/${username}`,
 };
 
 export const profilePaths = {
-    CreateProfile: `${serviceUrls.baseUrl}/api/v1/profile/create`,
-    GetProfile: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/profile/get/${profileId}`,
-    UpdateProfile: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/profile/update/${profileId}`,
-    DeleteProfile: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/profile/delete/${profileId}`,
+    CreateProfile: `${serviceUrls.baseUrl}${apiVersionV1}/profile/create`,
+    GetProfile: (profileId: string) => `${serviceUrls.baseUrl}${apiVersionV1}/profile/get/${profileId}`,
+    UpdateProfile: (profileId: string) => `${serviceUrls.baseUrl}${apiVersionV1}/profile/update/${profileId}`,
+    DeleteProfile: (profileId: string) => `${serviceUrls.baseUrl}${apiVersionV1}/profile/delete/${profileId}`,
+};
+
+export const experiencePaths = {
+    CreateExperience: (profileId: string) => `${serviceUrls.baseUrl}${apiVersionV1}/experience/create/${profileId}`,
+    GetExperience: (id: string) => `${serviceUrls.baseUrl}${apiVersionV1}/experience/get/${id}`,
+    GetAllExperience: (profileId: string) => `${serviceUrls.baseUrl}${apiVersionV1}/experience/getAll/${profileId}`,
+    UpdateExperience: (id: string) => `${serviceUrls.baseUrl}${apiVersionV1}/experience/update/${id}`,
+    DeleteExperience: (id: string) => `${serviceUrls.baseUrl}${apiVersionV1}/experience/delete/${id}`,
 };
 
 export const contactMePaths = {
-    CreateContactForm: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/create/${profileId}`,
-    GetContactForms: (profileId: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/getForms/${profileId}`,
-    ReplyContactForm: (id: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/reply/${id}`,
-    DeleteContactForm: (id: string) => `${serviceUrls.baseUrl}/api/v1/contactMe/delete/${id}`,
+    CreateContactForm: (profileId: string) => `${serviceUrls.baseUrl}${apiVersionV1}/contactMe/create/${profileId}`,
+    GetContactForms: (profileId: string) => `${serviceUrls.baseUrl}${apiVersionV1}/contactMe/getForms/${profileId}`,
+    ReplyContactForm: (id: string) => `${serviceUrls.baseUrl}${apiVersionV1}/contactMe/reply/${id}`,
+    DeleteContactForm: (id: string) => `${serviceUrls.baseUrl}${apiVersionV1}/contactMe/delete/${id}`,
 };
