0459ce1338910625055125b0fececadbafdd64e4
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-08-12 16:02:20 +0530

    Added Navbar Element To The Admin Layout

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -7,22 +7,28 @@ import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 
 interface Props {
+    navbarElement?: React.ReactNode;
     childElement?: React.ReactNode;
 }
 
 const AdminLayout: React.FC<Props> = (props) => {
 
-    const { childElement } = props;
+    const { navbarElement, childElement } = props;
 
     const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
 
     return (
         <Grid container>
+            {navbarElement &&
+                <Grid item xs={12}>
+                    {navbarElement}
+                </Grid>
+            }
             <Grid container className={`${(isXs || isSm || isMd) ? 'mx-5 my-3' : 'mx-10 my-10'}`} id='section-admin-home'>
                 {childElement}
             </Grid>
             <ScrollToTop source="admin" scrollToIdLarge="section-admin-home" scrollToIdSmall="section-admin-home" />
-        </Grid >
+        </Grid>
     );
 };
 
