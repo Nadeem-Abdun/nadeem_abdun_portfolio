a20b915f8124aee629a3d662588746df0fa6bdbc
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-09-13 19:21:49 +0530

    Added Bootstrap Icon Render Condition

--- src/components/SvgIconProvider.tsx
+++ src/components/SvgIconProvider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HtmlIcon, CssIcon, JavascriptIcon, TypescriptIcon, ReactIcon, ReactNativeIcon, JavaIcon, NodeJsIcon, ExpressJsIcon, MaterialUiIcon, MongoDbIcon, MySqlIcon, TailwindCssIcon, ReduxIcon, AngularIcon, VueIcon, SvelteIcon, SpringIcon, SpringBootIcon, PythonIcon, DjangoIcon, FlaskIcon } from "../assets/SvgIcons";
+import { HtmlIcon, CssIcon, JavascriptIcon, TypescriptIcon, ReactIcon, ReactNativeIcon, JavaIcon, NodeJsIcon, ExpressJsIcon, MaterialUiIcon, MongoDbIcon, MySqlIcon, TailwindCssIcon, ReduxIcon, AngularIcon, VueIcon, SvelteIcon, SpringIcon, SpringBootIcon, PythonIcon, DjangoIcon, FlaskIcon, BootstrapIcon } from "../assets/SvgIcons";
 
 interface IconProps {
     iconReference?: string;
@@ -59,6 +59,9 @@ const SvgIconProvider: React.FC<IconProps> = ({ iconReference }) => {
     }
     else if (iconReference === "MaterialUiIcon") {
         return <MaterialUiIcon aria-label="Material_UI" />;
+    }
+    else if (iconReference === "BootstrapIcon") {
+        return <BootstrapIcon aria-label="Bootstrap" />;
     }
     else if (iconReference === "MongoDbIcon") {
         return <MongoDbIcon aria-label="Mongo_Db" />;
