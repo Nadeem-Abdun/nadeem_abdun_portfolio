e36bc4a71b09ecd4ae44b5031781fc65c0ae0bfb
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-10-02 12:21:00 +0530

    UI Fixes And Improvements In Resume Page

--- src/screens/AppScreens/Resume.tsx
+++ src/screens/AppScreens/Resume.tsx
@@ -7,13 +7,13 @@ const Resume = () => {
     const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
 
     return (
-        <div id='section-resume' className={`flex flex-col justify-center items-start ${(isXs || isSm || isMd) ? 'my-3' : 'my-10'} h-full`}>
-            <Grid container xs={12} justifyContent='flex-start' rowGap={2} className="h-full">
+        <div id='section-resume' className={`flex flex-col justify-center items-start ${(isXs || isSm || isMd) ? 'my-3' : 'my-10'} h-[90%]`}>
+            <Grid container justifyContent='flex-start' rowGap={2} className="h-full">
                 <Grid item xs={12} className="h-[5%]">
                     <Typography variant="h4" fontWeight={500} fontFamily='inter'>Resume</Typography>
                 </Grid>
-                <Grid container item xs={12} justifyContent='center' className={`${(!isXs) && 'card'} ${(!isXs) && 'px-3'} ${(!isXs) && 'py-3'} h-[80%]`}>
-                    <Grid item xs={12} className="h-[95%]">
+                <Grid container item rowGap={2} className={`${(!isXs) && 'card'} ${(!isXs) && 'px-3'} ${(!isXs) && 'py-3'} h-[95%]`}>
+                    <Grid item xs={12} className={`${(isXs) ? "h-[75%]" : "h-[95%]"}`}>
                         <iframe
                             src="https://drive.google.com/file/d/1Df_7kkdNqnadwSUrBAFTM2JMTUB8179E/preview"
                             frameBorder="0"
@@ -22,7 +22,7 @@ const Resume = () => {
                             width="100%"
                         />
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={12} className={`${(isXs) ? "h-[25%]" : "h-[5%]"}`}>
                         <Button variant="contained" color="info" fullWidth>Download</Button>
                     </Grid>
                 </Grid>
