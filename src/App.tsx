cfcb5afef18b8f53c18d6ac2471bd5fea33d43ac
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-01-21 07:30:28 +0530

    Migration - CRA to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import AppLayout from "./layout/AppLayout";
-import AdminLayout from "./layout/AdminLayout";
-import AboutMe from "./screens/AppScreens/AboutMe";
-import Experience from "./screens/AppScreens/Experience";
-import Resume from "./screens/AppScreens/Resume";
-import Projects from "./screens/AppScreens/Projects";
-import ContactMe from "./screens/AppScreens/ContactMe";
-import WebNavigation from "./screens/AppScreens/WebNavigation";
-import MobileNavigation from "./screens/AppScreens/MobileNavigation";
-import WallOfCode from "./screens/AppScreens/WallOfCode";
-import Summary from "./screens/AppScreens/Summary";
-import TopNavBar from "./components/TopNavBar";
-import AdminPanel from "./screens/AdminScreens/AdminPanel";
-import AdminSignup from "./screens/AdminScreens/AdminSignup";
-import AdminLogin from "./screens/AdminScreens/AdminLogin";
-import AdminHome from "./screens/AdminScreens/AdminHome";
-import AdminProfile from "./screens/AdminScreens/AdminProfile";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import AdminLayout from './layout/AdminLayout';
+import AboutMe from './screens/AppScreens/AboutMe';
+import Experience from './screens/AppScreens/Experience';
+import Resume from './screens/AppScreens/Resume';
+import Projects from './screens/AppScreens/Projects';
+import ContactMe from './screens/AppScreens/ContactMe';
+import WebNavigation from './screens/AppScreens/WebNavigation';
+import MobileNavigation from './screens/AppScreens/MobileNavigation';
+import WallOfCode from './screens/AppScreens/WallOfCode';
+import Summary from './screens/AppScreens/Summary';
+import TopNavBar from './components/TopNavBar';
+import AdminPanel from './screens/AdminScreens/AdminPanel';
+import AdminSignup from './screens/AdminScreens/AdminSignup';
+import AdminLogin from './screens/AdminScreens/AdminLogin';
+import AdminHome from './screens/AdminScreens/AdminHome';
+import AdminProfile from './screens/AdminScreens/AdminProfile';
 
 const App = () => {
   const rightElementsArray = [
@@ -26,11 +26,11 @@ const App = () => {
     <WallOfCode key="wallofcode" />,
     <Projects key="projects" />,
     <ContactMe key="contactMe" />,
-    <Summary key="summary" />
+    <Summary key="summary" />,
   ];
 
   // Event tracker for the cursor light background
-  document.addEventListener('mousemove', (e) => {
+  document.addEventListener('mousemove', e => {
     document.documentElement.style.setProperty('--cursor-x', e.clientX + 'px');
     document.documentElement.style.setProperty('--cursor-y', e.clientY + 'px');
   });
@@ -38,42 +38,60 @@ const App = () => {
   return (
     <div className="min-h-screen min-w-full bg-darkblue text-gray-300">
       <Routes>
-        <Route path='/' element={
-          <AppLayout
-            leftElement={<WebNavigation />}
-            rightElements={rightElementsArray}
-            mobileElement={<MobileNavigation />}
-          />}
+        <Route
+          path="/"
+          element={
+            <AppLayout
+              leftElement={<WebNavigation />}
+              rightElements={rightElementsArray}
+              mobileElement={<MobileNavigation />}
+            />
+          }
         />
-        <Route path='/admin' element={
-          <AdminLayout
-            navbarElement={<TopNavBar />}
-            childElement={<AdminPanel />}
-          />}
+        <Route
+          path="/admin"
+          element={
+            <AdminLayout
+              navbarElement={<TopNavBar />}
+              childElement={<AdminPanel />}
+            />
+          }
         />
-        <Route path='/admin/signup' element={
-          <AdminLayout
-            navbarElement={<TopNavBar />}
-            childElement={<AdminSignup />}
-          />}
+        <Route
+          path="/admin/signup"
+          element={
+            <AdminLayout
+              navbarElement={<TopNavBar />}
+              childElement={<AdminSignup />}
+            />
+          }
         />
-        <Route path='/admin/login' element={
-          <AdminLayout
-            navbarElement={<TopNavBar />}
-            childElement={<AdminLogin />}
-          />}
+        <Route
+          path="/admin/login"
+          element={
+            <AdminLayout
+              navbarElement={<TopNavBar />}
+              childElement={<AdminLogin />}
+            />
+          }
         />
-        <Route path='/admin/profile' element={
-          <AdminLayout
-            navbarElement={<TopNavBar />}
-            childElement={<AdminProfile />}
-          />}
+        <Route
+          path="/admin/profile"
+          element={
+            <AdminLayout
+              navbarElement={<TopNavBar />}
+              childElement={<AdminProfile />}
+            />
+          }
         />
-        <Route path='/admin/home' element={
-          <AdminLayout
-            navbarElement={<TopNavBar />}
-            childElement={<AdminHome />}
-          />}
+        <Route
+          path="/admin/home"
+          element={
+            <AdminLayout
+              navbarElement={<TopNavBar />}
+              childElement={<AdminHome />}
+            />
+          }
         />
       </Routes>
     </div>
