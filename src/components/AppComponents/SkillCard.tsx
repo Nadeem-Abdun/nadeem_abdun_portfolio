f9f3292d4120289f3b53e6d13329847907f12459
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-02-11 11:50:06 +0530

    Migration - CRA to Vite

--- src/components/AppComponents/SkillCard.tsx
+++ src/components/AppComponents/SkillCard.tsx
@@ -1,24 +1,35 @@
-import React from "react";
-import { Grid, Typography } from "@mui/material";
-import SvgIconProvider from "../SvgIconProvider";
-import type { WallOfCode } from "../../redux/wallOfCode/wallOfCodeSlice";
+import React from 'react';
+import { Grid, Typography } from '@mui/material';
+import SvgIconProvider from '../SvgIconProvider';
+import type { WallOfCode } from '../../redux/wallOfCode/wallOfCodeSlice';
 
-const SkillCard: React.FC<WallOfCode> = (props) => {
+const SkillCard: React.FC<WallOfCode> = props => {
+  const { skillName, skillIcon } = props;
 
-    const { skillName, skillIcon } = props;
-    
-    return (
-        <div className="skill-card px-1 py-1">
-            <Grid container xs={12} justifyContent='center' alignItems='center'>
-                <Grid item>
-                    <SvgIconProvider iconReference={skillIcon} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography className="text-center" variant="body1" fontWeight={400} fontFamily='inter'>{skillName}</Typography>
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+  return (
+    <div className="skill-card px-1 py-1">
+      <Grid
+        container
+        size={{ xs: 12 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid>
+          <SvgIconProvider iconReference={skillIcon} />
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          <Typography
+            className="text-center"
+            variant="body1"
+            fontWeight={400}
+            fontFamily="inter"
+          >
+            {skillName}
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default SkillCard
+export default SkillCard;
