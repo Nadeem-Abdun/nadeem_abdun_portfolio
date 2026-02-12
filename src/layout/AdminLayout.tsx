da8f48d2bfabb41e0dd41fa212e0b4201e717044
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-02-12 11:51:03 +0530

    Migration - CRA to Vite

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -1,35 +1,38 @@
-import React from "react";
-import { Grid } from "@mui/material";
-import { useBreakpoints } from "../utils/Breakpoints";
-import ScrollToTop from "../components/ScrollToTop";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
+import React from 'react';
+import { Grid } from '@mui/material';
+import { useBreakpoints } from '../utils/Breakpoints';
+import ScrollToTop from '../components/ScrollToTop';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
 
 interface Props {
-    navbarElement?: React.ReactNode;
-    childElement?: React.ReactNode;
+  navbarElement?: React.ReactNode;
+  childElement?: React.ReactNode;
 }
 
-const AdminLayout: React.FC<Props> = (props) => {
+const AdminLayout: React.FC<Props> = props => {
+  const { navbarElement, childElement } = props;
 
-    const { navbarElement, childElement } = props;
+  const { isMd, isSm, isXs } = useBreakpoints();
 
-    const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
-
-    return (
-        <Grid container>
-            {navbarElement &&
-                <Grid item xs={12}>
-                    {navbarElement}
-                </Grid>
-            }
-            <Grid container className={`${(isXs || isSm || isMd) ? 'mx-5 my-3' : 'mx-10 my-10'}`} id='section-admin-home'>
-                {childElement}
-            </Grid>
-            <ScrollToTop source="admin" scrollToIdLarge="section-admin-home" scrollToIdSmall="section-admin-home" />
-        </Grid>
-    );
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      {navbarElement && <Grid size={{ xs: 12 }}>{navbarElement}</Grid>}
+      <Grid
+        container
+        className={`${isXs || isSm || isMd ? 'mx-5 my-3' : 'mx-10 my-10'}`}
+        id="section-admin-home"
+      >
+        {childElement}
+      </Grid>
+      <ScrollToTop
+        source="admin"
+        scrollToIdLarge="section-admin-home"
+        scrollToIdSmall="section-admin-home"
+      />
+    </Grid>
+  );
 };
 
 export default AdminLayout;
