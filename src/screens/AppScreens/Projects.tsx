89866a9d290f4ef9f59fa4cac1c23b4010f5318f
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-02-28 11:28:04 +0530

    Migration - CRA to Vite

--- src/screens/AppScreens/Projects.tsx
+++ src/screens/AppScreens/Projects.tsx
@@ -1,44 +1,66 @@
-import React from "react";
-import { Grid, Typography } from "@mui/material";
-import { useBreakpoints } from "../../utils/Breakpoints";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../redux/store";
-import ProjectCard from "../../components/AppComponents/ProjectCard";
+import { Grid, Typography } from '@mui/material';
+import { useBreakpoints } from '../../utils/Breakpoints';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import ProjectCard from '../../components/AppComponents/ProjectCard';
 
 const Projects = () => {
+  const { isMd, isSm, isXs } = useBreakpoints();
 
-    const { isXl, isLg, isMd, isSm, isXs } = useBreakpoints();
+  const { projects } = useSelector((state: RootState) => state.project);
 
-    const { projects } = useSelector((state: RootState) => state.project);
-    const dispatch = useDispatch();
-
-    return (
-        <div id='section-projects' className={`flex flex-col justify-center items-center ${(isXs || isSm || isMd) ? 'my-3' : 'my-10'}`}>
-            <Grid container xs={12} justifyContent='flex-start' alignItems='center' rowSpacing={2}>
-                <Grid item xs={12}>
-                    <Typography variant="h4" fontWeight={500} fontFamily='inter'>Projects</Typography>
+  return (
+    <div
+      id="section-projects"
+      className={`flex flex-col justify-center items-center ${isXs || isSm || isMd ? 'my-3' : 'my-10'}`}
+    >
+      <Grid
+        container
+        size={{ xs: 12 }}
+        justifyContent="flex-start"
+        alignItems="center"
+        rowSpacing={2}
+      >
+        <Grid size={{ xs: 12 }}>
+          <Typography variant="h4" fontWeight={500} fontFamily="inter">
+            Projects
+          </Typography>
+        </Grid>
+        <Grid
+          container
+          size={{ xs: 12 }}
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          rowGap={1}
+        >
+          {projects &&
+            projects.map((project, index) => {
+              const {
+                projectPicture,
+                title,
+                description,
+                skillsInvolved,
+                websiteUrl,
+                repositoryUrl,
+              } = project;
+              return (
+                <Grid key={index} size={{ xs: 12 }}>
+                  <ProjectCard
+                    key={index}
+                    projectPicture={projectPicture}
+                    title={title}
+                    description={description}
+                    skillsInvolved={skillsInvolved}
+                    websiteUrl={websiteUrl}
+                    repositoryUrl={repositoryUrl}
+                  />
                 </Grid>
-                <Grid container item xs={12} justifyContent="flex-start" alignItems="flex-start" rowGap={1}>
-                    {projects && projects.map((project, index) => {
-                        const { projectPicture, title, description, skillsInvolved, websiteUrl, repositoryUrl } = project;
-                        return (
-                            <Grid item xs={12}>
-                                <ProjectCard
-                                    key={index}
-                                    projectPicture={projectPicture}
-                                    title={title}
-                                    description={description}
-                                    skillsInvolved={skillsInvolved}
-                                    websiteUrl={websiteUrl}
-                                    repositoryUrl={repositoryUrl}
-                                />
-                            </Grid>
-                        )
-                    })}
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+              );
+            })}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
