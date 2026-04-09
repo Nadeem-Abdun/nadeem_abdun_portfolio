b4706313a3d6a70daeecbb24e9a166c2c82c8766
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-04-09 15:58:10 +0530

    Migration - CRA to Vite

--- src/utils/SecureHttp.ts
+++ src/utils/SecureHttp.ts
@@ -1,13 +1,14 @@
 export const SecureHttp = (url: string): string => {
-    if (!url) return url;
-    try {
-        const parsedUrl = new URL(url);
-        if (parsedUrl.protocol === "http:") {
-            parsedUrl.protocol = "https:";
-        }
-        return parsedUrl.toString();
-    } catch (error) {
-        console.error("Invalid URL:", url, error);
-        return url;
+  if (!url) return url;
+  if (import.meta.env.DEV) return url;
+  try {
+    const parsedUrl = new URL(url);
+    if (parsedUrl.protocol === 'http:') {
+      parsedUrl.protocol = 'https:';
     }
+    return parsedUrl.toString();
+  } catch (error) {
+    console.error('Invalid URL:', url, error);
+    return url;
+  }
 };
