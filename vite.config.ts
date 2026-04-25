39c31dfaaa7811d9897afba9fb9f3d7c5c4cdd3e
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-04-26 04:33:30 +0530

    Github Pages Deployment Workflow Added

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,29 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import type { ViteUserConfig } from 'vitest/config';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    minify: 'terser',
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-  },
-} as ViteUserConfig);
+export default defineConfig(
+  ({ command }) =>
+    ({
+      base: command === 'build' ? '/nadeem_abdun_portfolio/' : '/',
+      plugins: [react(), tailwindcss()],
+      server: {
+        port: 3000,
+        open: true,
+      },
+      build: {
+        outDir: 'dist',
+        minify: 'terser',
+        sourcemap: true,
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        },
+      },
+      test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './src/test/setup.ts',
+      },
+    }) as ViteUserConfig
+);
